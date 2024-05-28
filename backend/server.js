124aff62f3bf2e17c7bf400dcc386c08a7422370
Cesar Alexandre <cesaralexandreprofissional@gmail.com>
2024-05-28 13:52:51 +0100

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import { app, server } from './socket/socket.js';
 import dotenv from 'dotenv';
@@ -16,15 +17,20 @@ app.use(express.json())
 app.use(cookieParser())
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!!!!!!');
-})
+
 
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
